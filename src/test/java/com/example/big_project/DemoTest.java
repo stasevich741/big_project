2d8ad5f7c0623ae90e931a1fe42ddcebf7ed07da
Stanislav Korolkov <stasevich741@gmail.com>
2023-02-22 18:15:05 +0300

    init

--- src/test/java/com/example/big_project/DemoTest.java
+++ src/test/java/com/example/big_project/DemoTest.java
@@ -2,9 +2,14 @@ package com.example.big_project;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,13 +24,15 @@ class DemoTest {
     @Test
     void simpleTestForChrome() {
         driver = new ChromeDriver();
-        driver.get("https://google.com");
-        driver.navigate().to("https://yahoo.com");
-        driver.navigate().back();
-        driver.navigate().forward();
-        driver.navigate().refresh();
+        driver.get("https://login.salesforce.com/");
+        WebElement element = driver.findElement(By.cssSelector("input[id='username']"));
+        element.sendKeys("123");
+        element.submit();
 
-        assertThat(driver.getCurrentUrl()).isEqualTo("https://www.yahoo.com/");
+//        String errorText = driver.findElement(By.cssSelector("div#error.loginError")).getText();
+//        String errorText = driver.findElement(By.id("error")).getText();
+        String errorText = driver.findElement(By.xpath("//*[@id='error']")).getText();
+        assertThat(errorText).isEqualTo("Please enter your password.");
     }
 
     @Test
@@ -38,9 +45,9 @@ class DemoTest {
         System.out.println(driver.getTitle());
     }
 
-    @AfterEach
-    void close() {
-        driver.quit();
-    }
+//    @AfterEach
+//    void close() {
+//        driver.quit();
+//    }
 }
 
